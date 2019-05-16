e2860e8cf76fc00bf469647b87d09e9a2257d882
Kirvyteo <44790247+Kirvyteo@users.noreply.github.com>
2019-05-16 10:47:34 +0800

    Update start_intime_vivado_linux.tcl

--- scripts/tcl_console/start_intime_vivado_linux.tcl
+++ scripts/tcl_console/start_intime_vivado_linux.tcl
@@ -12,8 +12,8 @@
 #
 # Specify InTime and project paths
 #
-set yourintimetoolpath "/home/dev2002/intime_v2.6.11_61343cb/intime.sh"
-set yourproject "/home/dev2002/plunify/examples/vivado/eight_bit_uc_xpr/eight_bit_uc.xpr"
+set yourintimetoolpath "/home/dev2002/intime_v2.6.11/intime.sh"
+set yourproject "/home/plunify/examples/vivado/eight_bit_uc_xpr/eight_bit_uc.xpr"
 #
 # Specify your FPGA tool version
 # The following assumes that you have registered your tool in InTime. (If not, refer to https://docs.plunify.com/intime/configuration.html)
@@ -23,9 +23,9 @@ set yourtoolchain "2018.2.0"
 #
 # Specify the InTime Tcl script 
 # To understand more about the Tcl API, refer to Flow Properties - https://docs.plunify.com/intime/flow_properties.html
-# Download a sample from https://github.com/plunify/InTime
+# Download a sample from https://github.com/plunify/InTime-Vivado/tree/master/scripts/intime
 #
-set yourintimescript "/home/dev2002/run_hotstart.tcl"
+set yourintimescript "intimeflow.tcl"
 
 
 #################################################################################
