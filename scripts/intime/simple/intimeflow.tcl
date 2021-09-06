e4f4961c86a9327a5294a8104dd11ae480379498
Harn Hua Ng <harnhua@users.noreply.github.com>
2021-09-06 14:49:52 +0800

    Added descriptive comments

--- scripts/intime/simple/intimeflow.tcl
+++ scripts/intime/simple/intimeflow.tcl
@@ -1,9 +1,28 @@
+#----------------------------------------------------------------------
+# Script      : intimeflow.tcl
+# Description : Runs an InTime recipe
+# Author      : Plunify
+# Version     : 1.0
+#               
+# Return : None
+#
+# Usage :
+#    1. Via the InTime Tcl console
+#       % source <script.tcl>
+#
+#    2. Via Command-line
+#       % intime.sh -mode batch -platform minimal -s <script.tcl>
+#----------------------------------------------------------------------
+
 # Sample script for InTime Optimization. Works Windows & Linux
 flow reset
+# Build machine(s): local or private_cloud or plunify_cloud
 flow set run_target local
-flow set runs_per_round 2
+# Number of generated strategies
+flow set runs_per_round 40
 flow set rounds 1
-flow set concurrent_runs 1
+# Number of builds in parallel
+flow set concurrent_runs 8
 flow set control_create_bitstreams false
 flow load_recipe "hot_start"
 flow run_recipe "hot_start"
